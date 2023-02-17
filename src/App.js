5fd1feff21e940962c9159234263f90a77342698
unknown <Gejjeg>
2023-02-17 21:21:00 +0100

    feat: Component Composition - 01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container } from "./Container";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export class App extends React.Component {
+    render() {
+        return (
+            <Container>
+                <h1>Container Class</h1>
+                <p>A child of the Container Class pass as a children props which is automatically set by react</p>
+            </Container>
+        )
+    }
+};
