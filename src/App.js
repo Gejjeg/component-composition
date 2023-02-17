50416ad0e8a13d4cbc516bc50bef3588ea9e19b2
unknown <Gejjeg>
2023-02-17 21:33:22 +0100

    feat: Component Composition - 02

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import { Container } from "./Container";
 export class App extends React.Component {
     render() {
         return (
-            <Container>
-                <h1>Container Class</h1>
+            <Container title={<h1>Container Class</h1>}>
                 <p>A child of the Container Class pass as a children props which is automatically set by react</p>
             </Container>
         )
